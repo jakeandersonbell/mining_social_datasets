5e3667d7b6e7002d003a7dbb2cc9c6b7fcd59607
jakeandersonbell <jakeandersonbell@gmail.com>
2020-03-16 10:23:40 +0000

    Code for task 1 completed

---
+++ task2/task2.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+from sklearn.datasets import fetch_california_housing
+
+cal_housing = fetch_california_housing()
+X = pd.DataFrame(cal_housing.data, columns=cal_housing.feature_names)
+y = cal_housing.target

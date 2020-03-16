72674ea57851b531f4bfdd17c4c61cfcd687169b
jakeandersonbell <jakeandersonbell@gmail.com>
2020-03-16 10:25:12 +0000

    Code for task 1 completed

--- task2/task2.py
+++ task2/task2.py
@@ -4,4 +4,5 @@ from sklearn.datasets import fetch_california_housing
 
 cal_housing = fetch_california_housing()
 X = pd.DataFrame(cal_housing.data, columns=cal_housing.feature_names)
+
 y = cal_housing.target
